8d3b97e5550ff68d4e39e25ea6ca20cb6bf1b2a9
fayssal tahtoub <fayssa.tahtoub@gmail.com>
2014-03-04 02:23:13 +0000

    use mysql connector

--- publicat/publicat/index.aspx.cs
+++ publicat/publicat/index.aspx.cs
@@ -4,14 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System;
+using System.Data;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace publicat
 {
     public partial class index : System.Web.UI.Page
-    {
+    { 
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            string connStr = "server=localhost;user=root;database=world;port=3306;password=001985;";
+            MySqlConnection conn = new MySqlConnection(connStr);
         }
     }
 }
